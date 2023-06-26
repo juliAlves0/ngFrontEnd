95d681be973cfa761f2d44fb90216fc01c824aad
Juliana Cezar <julialvesrangel@gmail.com>
2023-06-26 14:20:05 -0300

    versao pub

--- src/app/models/contact.model.ts
+++
@@ -1,9 +0,0 @@
-export class Contact {
-    id: Number = 1;
-    date: Date = new Date();
-    name: String = '';
-    email: String = '';
-    subject: String = '';
-    message: String = '';
-    status: String = '';
-}
--- src/app/models/thing.model.ts
+++
@@ -1,8 +0,0 @@
-export class Thing {
-    id: Number = 1;
-    date: Date = new Date();
-    name: String = '';
-    description: String = '';
-    location: String = '';
-    status: String = '';
-}
